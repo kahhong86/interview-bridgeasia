68b6f763cd34552268f70f615c070fa6632e5047
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2025-08-01 07:40:25 +0800

    added logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,13 +3,25 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FC } from "react";
 import styles from "./Header.module.css";
+import Image from "next/image";
 
 const Header: FC = () => {
   const pathname = usePathname();
 
   return (
     <header className="flex justify-between items-center py-4">
-      <Link href="/"><h1 className="text-2xl font-bold">Math Worksheet</h1></Link>
+      <Link href="/" className="flex items-center space-x-2">
+        <div className="logo bg-[#000] rounded-full w-[50px] h-[50px] flex items-center justify-center">
+          <Image
+          src="/images/logo-white.png"
+          alt="Logo"
+          width={20}
+          height={40}
+          className="h-10"
+        />
+        </div>
+        <h1 className="md:text-2xl text-lg font-bold">Math <br />Worksheet</h1>
+      </Link>
       <nav>
         <Link
           href="/"
