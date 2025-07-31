0fbda43a1c0806580d86e2adff65dbac7593c0aa
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2025-07-31 17:10:53 +0800

    Updated apikey private

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAk_h7HAq4W9ZavKAezRs_4BFOfJmOcL6U",
-  authDomain: "interview-42aa2.firebaseapp.com",
-  projectId: "interview-42aa2",
-  storageBucket: "interview-42aa2.appspot.com", 
-  messagingSenderId: "893545544063",
-  appId: "1:893545544063:web:42115c69c988377c95824a",
-  measurementId: "G-0NRB2BQXG1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
